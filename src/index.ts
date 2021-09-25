40dd1b4875bbd95309d254d132e36e2b76c28579
Marcos Moraes Pereira <meuid3@gmail.com>
2021-09-25 01:22:06 -0300

    chore: commit inicial do projeto

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  // ... you will write your Prisma Client queries here
+  const createUser = await prisma.user.create({
+    data: {
+      name: "Marcos Moraes Pereira",
+      email: "meuid3@gmail.com"
+    }
+  })
+
+  const allUsers = await prisma.user.findMany()
+  console.log(allUsers)
+}
+
+main()
