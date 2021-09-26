918976f168b7f0bdb3fc667939c70e5812342537
Marcos Moraes Pereira <meuid3@gmail.com>
2021-09-26 01:44:40 -0300

    feat: adicionado método de buscar usuário por email no repositorio de UsuarioRepository

--- src/repositories/UsuarioRepository.ts
+++ src/repositories/UsuarioRepository.ts
@@ -3,14 +3,22 @@ import { PrismaClient } from '@prisma/client'
 import { IUsuarioRepository } from "./adapters/IUsuarioRepository"
 
 export class UsuarioRepository implements IUsuarioRepository {
-  
   private prisma = new PrismaClient()
 
   async cadastrar(usuario: Usuario):Promise<Usuario>  {
-
     const persist = await this.prisma.usuario.create({
       data: usuario
     })
     return persist
+  }
+
+  async buscarUsuarioPorEmail(email: string): Promise<Usuario | null> {
+    const usuario = await this.prisma.usuario.findUnique({
+      where: {
+        email: email
+      }
+    })
+
+    return usuario
   }
 }
