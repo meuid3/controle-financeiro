0c87e724af9d2444673cd10703dfb33d710bc161
Marcos Moraes Pereira <meuid3@gmail.com>
2021-09-26 01:05:04 -0300

    feat: adicionado validações na criação de usuário

--- src/usecases/cadastrarUsuario/CadastrarUsuarioUseCase.ts
+++ src/usecases/cadastrarUsuario/CadastrarUsuarioUseCase.ts
@@ -1,16 +1,42 @@
-import { Usuario } from "../../entities/Usuario";
-import { IUsuarioRepository } from "../../repositories/adapters/IUsuarioRepository";
+import { Usuario } from "../../entities/Usuario"
+import { UsuarioRequestDTO } from "./UsuarioRequestDTO"
+import { IUsuarioRepository } from "../../repositories/adapters/IUsuarioRepository"
 
 export class CadastrarUsuarioUseCase {
   
   constructor(private usuarioRepository: IUsuarioRepository){}
 
-  public async handle(usuario: Usuario): Promise<any> {
-      
-    if(!usuario) {
-      throw new Error('Dados não informados')
-    }
-    
-    return await this.usuarioRepository.cadastrar(usuario)
+  public async handle(usuarioRequest: UsuarioRequestDTO): Promise<Usuario> {
+    this.verificaCamposObrigatorios(usuarioRequest)
+    this.verificaSenha(usuarioRequest)
+
+    const {nome, email, senha} = usuarioRequest
+
+    return await this.usuarioRepository.cadastrar({
+      nome,
+      email,
+      senha
+    })
   }
+
+  private verificaSenha({senha, confirmarSenha}: UsuarioRequestDTO): void {
+    if(senha !== confirmarSenha)
+      throw new Error("As senhas não conferem")
+  }
+
+  private verificaCamposObrigatorios(usuario: any): void {
+    const camposObrigatorios = [
+      'nome',
+      'email',
+      'senha',
+      'confirmarSenha'
+    ]
+
+    for(let campo of camposObrigatorios) {
+      if(!usuario[campo]) {
+        throw new Error(`Campo ${campo} não informado!`)
+      }
+    }
+  }
+    
 }
