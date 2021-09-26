a3ab03c076fc6873bf99906d00058ee8eaf602c4
Marcos Moraes Pereira <meuid3@gmail.com>
2021-09-26 01:44:07 -0300

    feat: adicionado método de buscar usuário por email no adaptador de UsuarioRepository

--- src/repositories/adapters/IUsuarioRepository.ts
+++ src/repositories/adapters/IUsuarioRepository.ts
@@ -2,4 +2,5 @@ import { Usuario } from '../../entities/Usuario'
 
 export interface IUsuarioRepository {
   cadastrar(usuario: Usuario): Promise<Usuario>
+  buscarUsuarioPorEmail(email: string): Promise<Usuario | null>
 }
