a877f99019bb00ce74312cb62da0aeead5a5c9f8
Marcos Moraes Pereira <meuid3@gmail.com>
2021-09-26 23:12:13 -0300

    feat: adicionado metodo que lista usuários no controller de Usuarios

--- src/controllers/usuarioController.ts
+++ src/controllers/usuarioController.ts
@@ -1,14 +1,15 @@
 import { Request, Response } from 'express'
 import { UsuarioRepository } from '../repositories/UsuarioRepository'
 import { CadastrarUsuarioService } from '../usecases/cadastrarUsuario/CadastrarUsuarioService'
+import { ListarUsuarioService } from '../usecases/listarUsuario/ListarUsuarioService'
 
 class UsuarioController {
 
   async cadastrarUsuario(request: Request, response: Response) {
     try {
       const usuario = request.body
-      const usuarioRepository = new UsuarioRepository()
-      const cadastrarUsuarioService = new CadastrarUsuarioService(usuarioRepository)
+      
+      const cadastrarUsuarioService = new CadastrarUsuarioService(usuarioRepository())
       const usuarioCadastrado = await cadastrarUsuarioService.handle(usuario)
       response.json(usuarioCadastrado)
     }
@@ -17,6 +18,22 @@ class UsuarioController {
       response.status(500).json({message: msg})
     }
   }
+
+  async listarUsuario(request: Request, response: Response) {
+    try {
+      const listarUsuarioService = new ListarUsuarioService(usuarioRepository())
+      const usuarios = await listarUsuarioService.handle()
+      response.json(usuarios)
+
+    } catch(error) {
+      const  msg = (error as Error).message;
+      response.status(500).json({message: msg})
+    }
+  }
+}
+
+const usuarioRepository = () => {
+  return new UsuarioRepository()
 }
 
 export default new UsuarioController()
