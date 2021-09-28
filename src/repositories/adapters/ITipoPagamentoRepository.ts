953df46ed298a0d4a4c02328a83f0f08efb2cb11
Marcos Moraes Pereira <meuid3@gmail.com>
2021-09-27 21:59:55 -0300

    feat: adicionado adapter de tipoPagamento

---
+++ src/repositories/adapters/ITipoPagamentoRepository.ts
@@ -0,0 +1,8 @@
+import { TipoPagamento } from '../../entities/TipoPagamento'
+
+export interface ITipoPagamentoRepository {
+  cadastrar(tipoPagamento: TipoPagamento): Promise <TipoPagamento>
+  listarTiposPagamento(): Promise <TipoPagamento>
+  ativarTipoPagamento(): Promise <TipoPagamento>
+  inativarTipoPagamento(): Promise <TipoPagamento>
+}
