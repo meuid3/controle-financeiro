6d56b1dfe418354a0317771482f5245e945fd2c5
Marcos Moraes Pereira <meuid3@gmail.com>
2021-09-28 01:13:07 -0300

    feat: adicionado metodo de listar categorias no controle de Categorias

--- src/controllers/categoriaController.ts
+++ src/controllers/categoriaController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express'
 import { CategoriaRepository } from '../repositories/CategoriaRepository'
+import { ListarCategoriasService } from '../usecases/listarCategorias/ListarCategoriasService'
 import { CadastrarCategoriaService } from '../usecases/cadastrarCategoria/CadastrarCategoriaService'
 
 class CategoriaContoller {
@@ -10,6 +11,18 @@ class CategoriaContoller {
       const categoriaCadastrada = await categoriaService.executar(categoria)
       response.json(categoriaCadastrada)
       
+    } catch(error) {
+      const msg = (error as Error).message
+      response.status(500).json({message: msg})
+    }
+  }
+
+  public async listarCategorias(request: Request, response: Response) {
+    try {
+      const categoriaService = new ListarCategoriasService(categoriaRepository())
+      const categorias = await categoriaService.executar()
+      response.json(categorias)
+
     } catch(error) {
       const msg = (error as Error).message
       response.status(500).json({message: msg})
