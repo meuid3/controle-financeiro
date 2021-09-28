13cdd9140ef5b740c56820fd6f0f6a8be73e9088
Marcos Moraes Pereira <meuid3@gmail.com>
2021-09-28 00:50:50 -0300

    feat: adicionado controller de Categoria

---
+++ src/controllers/categoriaController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express'
+import { CategoriaRepository } from '../repositories/CategoriaRepository'
+import { CadastrarCategoriaService } from '../usecases/cadastrarCategoria/CadastrarCategoriaService'
+
+class CategoriaContoller {
+  public async cadastrar(request: Request, response: Response) {
+    try {
+      const categoria = request.body
+      const categoriaService = new CadastrarCategoriaService(categoriaRepository())
+      const categoriaCadastrada = await categoriaService.executar(categoria)
+      response.json(categoriaCadastrada)
+      
+    } catch(error) {
+      const msg = (error as Error).message
+      response.status(500).json({message: msg})
+    }
+  }
+}
+
+const categoriaRepository = () => {
+  return new CategoriaRepository()
+}
+
+export default new CategoriaContoller()
