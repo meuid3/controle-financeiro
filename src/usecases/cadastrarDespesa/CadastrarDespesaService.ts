1719d20aadbcd35a62c6fff93fa0036b4e9d9e43
Marcos Moraes Pereira <meuid3@gmail.com>
2021-09-29 02:01:22 -0300

    feat: tratando entrada do atributo dataCompra

--- src/usecases/cadastrarDespesa/CadastrarDespesaService.ts
+++ src/usecases/cadastrarDespesa/CadastrarDespesaService.ts
@@ -5,7 +5,9 @@ export class CadastrarDespesaService {
   
   constructor(private despesaRepository: IDespesaRepository) {}
 
-  public async executar(despesa: Despesa): Promise <Despesa> {
+  public async executar(despesa: Despesa): Promise <Despesa> { 
+    const dataCompra = new Date(despesa.dataCompra)
+    despesa.dataCompra = dataCompra
     const despesaCadastrada = await this.despesaRepository.cadastrar(despesa)
 
     return despesaCadastrada
