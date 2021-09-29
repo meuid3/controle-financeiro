54584ed176716f12ac6297ae1d99e1af22773a69
Marcos Moraes Pereira <meuid3@gmail.com>
2021-09-28 23:59:37 -0300

    fix: ajustando retorno dos metodos do adaptador de repositório

--- src/repositories/adapters/IDespesaRepository.ts
+++ src/repositories/adapters/IDespesaRepository.ts
@@ -1,8 +1,9 @@
 import { Categoria } from "../../entities/Categoria";
+import { Despesa } from "../../entities/Despesa";
 
 export interface IDespesaRepository {
-  cadastrar(): Promise <Categoria>
-  listarCategoria(): Promise <Categoria>
-  listarCategorias(): Promise <Categoria[]>
+  cadastrar(): Promise <Despesa>
+  listarCategoria(): Promise <Despesa>
+  listarCategorias(): Promise <Despesa[]>
   excluirCategoria(): Promise<void>
 }
