8b4e4d749317ab2f777783256be7d98e18e60513
Marcos Moraes Pereira <meuid3@gmail.com>
2021-09-29 01:49:17 -0300

    feat: declarando rota de listar despesas

--- src/routes/despesaRouter.ts
+++ src/routes/despesaRouter.ts
@@ -4,5 +4,6 @@ import DespesaController from '../controllers/despesaController'
 const despesaRouter = Router()
 
 despesaRouter.post('/cadastrar', DespesaController.cadastrar)
+despesaRouter.post('/', DespesaController.listarDespesas)
 
 export {despesaRouter}
